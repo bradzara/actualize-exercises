527650c5ccd7e97d48093b15e0d74b6d6db2e540
bradzara <bradzara93@gmail.com>
2024-04-01 22:54:45 -0700

    Create 112.rb

---
+++ 112.rb
@@ -0,0 +1,27 @@
+# QUESTION 1
+# Write code to print hellohellohello using string addition.
+
+puts "hello" + "hello" + "hello
+"
+# Then write code to print hellohellohello again, this time using string multiplication.
+
+puts "hello" * 3
+
+# QUESTION 2
+# Read the code below and predict which lines will crash. 
+# Then run the code and remove the invalid lines one at a time, until only the valid code remains.
+puts 9 + 3
+
+
+puts "9" + "3"
+puts 9 - 3
+
+
+puts 9 * 3
+puts "9" * 3
+
+
+puts 9 / 3
+
+
+
