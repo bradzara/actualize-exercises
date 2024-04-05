f902c76fd3f2fc9aa26a68ed2b27e24d71bc87fe
bradzara <bradzara93@gmail.com>
2024-04-05 09:06:16 -0700

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,34 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def lyrics_banner
+    puts '************************************************************************'
+    puts '* ____  ____  ____  ____  _  _        ____  ____  __  __ _   ___  ____ *'
+    puts '*(  __)(  _ \(  __)/ ___)/ )( \      (  _ \(  _ \(  )(  ( \ / __)(  __)*'
+    puts '* ) _)  )   / ) _) \___ \) __ (       ) __/ )   / )( /    /( (__  ) _) *'
+    puts '*(__)  (__\_)(____)(____/\_)(_/      (__)  (__\_)(__)\_)__) \___)(____)*'
+    puts '*        __  ____        ____  ____  __          __   __  ____         *'
+    puts '*       /  \(  __)      (  _ \(  __)(  )   ___  / _\ (  )(  _ \        *'
+    puts '*      (  O )) _)        ) _ ( ) _) / (_/\(___)/    \ )(  )   /        *'
+    puts '*       \__/(__)        (____/(____)\____/     \_/\_/(__)(__\_)        *'
+    puts '************************************************************************'
+end
+
+puts lyrics_banner
+puts lyrics_banner
+puts lyrics_banner
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def print_lyrics
+  puts "Now this is a story all about how"
+  puts "My life got flipped turned upside down"
+  puts "And I'd like to take a minute, just sit right there"
+  puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+
+puts print_lyrics
