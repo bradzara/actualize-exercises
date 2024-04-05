385544cb40df44974d83028c0d48dff82a38fc13
bradzara <bradzara93@gmail.com>
2024-04-04 21:57:26 -0700

    Create 141.rb

---
+++ 141.rb
@@ -0,0 +1,21 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+x = names[2]                           # 4
+names[0] = x                           # 5
+pp names                               # 6
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: Changing the second name in the array to "Bob"
+# 4: x is "Danilo"
+# 5: Changing the first name in the array to "Danilo"
+# 6: Printing the array
+
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 3, 2]
+pp items
