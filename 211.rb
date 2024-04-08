0ff2fd32760a839557f53f8ae36df5355d99972f
bradzara <bradzara93@gmail.com>
2024-04-08 13:28:29 -0700

    211.rb

---
+++ 211.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "deliberate"
+word2 = "practice"
+
+puts (word1 + ' ' + word2).upcase
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to do this from memory, although my solution was slightly different from the one provided
+# I put my string equation in parenthesis so I only had to use the upcase command once. I also inserted a space.
+# Tomorrow, I will be working on printing out the number of charcters in a word.
