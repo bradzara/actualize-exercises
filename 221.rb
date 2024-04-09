4aa876478cf23788efeaae5915cd5be116aac61b
bradzara <bradzara93@gmail.com>
2024-04-09 09:49:34 -0700

    Create 221.rb

---
+++ 221.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "alphabet"
+puts word.length
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve this from memory
+# Tommorow I will convery a float into an integer
