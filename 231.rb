fbf740586c59700accc1d1571389fabc713a9aba
bradzara <bradzara93@gmail.com>
2024-04-10 09:21:53 -0700

    Create 231.rb

---
+++ 231.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "CARPET"
+puts word.downcase
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve this from memory
+# Tomorrow I will be starting the next folder, practicing if statements
+
