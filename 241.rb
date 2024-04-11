eec7eb3e8c38a418cc4fb55ab04b592b71239818
bradzara <bradzara93@gmail.com>
2024-04-11 11:01:13 -0700

    Create 241.rb

---
+++ 241.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+number = 293847
+
+if number > 9000
+  puts 1
+else
+  puts -1
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Yes, I was able to solve from memory
+# Tomorrow, I will continue with "if" statments
