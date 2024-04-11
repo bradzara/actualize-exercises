5bdc14dd357ec12f6cd881d2ec28cbc993802921
bradzara <bradzara93@gmail.com>
2024-04-11 11:26:37 -0700

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,14 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+
+scenery = ["Mountain", "Beach", "Desert"]
+scenery.push("Forest")
+pp scenery
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+
+scenery_attributes = {"Mountain" => "snow", "Beach" => "water", "Desert" => "sand", "forest" => "trees"}
+pp scenery_attributes
