d6d26347f9d0a34e278fb0a341fb34165ead2441
bradzara <bradzara93@gmail.com>
2024-04-12 08:41:22 -0700

    Create 251.rb

---
+++ 251.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero 
+# and the second number is greater than 0, and prints 0 otherwise.
+
+number1 = 3
+number2 = 23
+
+if number1 < 0 && number2 > 0
+  puts 1
+else
+  puts 0
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to do this from memory
+# Tomorrow i will start practice with loops
+
+
+
