0b1296fa67bf23c08142be88a0c59cb71326b07d
bradzara <bradzara93@gmail.com>
2024-04-15 08:23:24 -0700

    Create 311.rb

---
+++ 311.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+cars = ["Ford", "Subaru", "Toyota"]
+cars << "BMW" << "Honda"
+pp cars
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve from memory, even though I used the symbols and not the .push method
+# Tomorrow, I will continue to work on arrays and hashes
