7c86b11c094b3336a47c982e0b5a7371f4da80e6
bradzara <bradzara93@gmail.com>
2024-04-16 08:33:13 -0700

    Create 321.rb

---
+++ 321.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+letters = ["a", "b", "c", "d"]
+letters[1] = 8
+p letters
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve from memory
+# Tomorrow, I will continue to work on arrays and hashes
