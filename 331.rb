f4baa09a1ceab54be9ba7c23035c8246cfd487a1
bradzara <bradzara93@gmail.com>
2024-04-17 09:06:44 -0700

    Create 331.rb

---
+++ 331.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [5, 9]
+index = 0
+while index < numbers.length
+  p numbers[index]
+  index = index + 1
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Yes, I was able to solve from memory
+# Tomorrow I will begin practicing hashes
