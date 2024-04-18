8aa9734f104ea6d309b84c9cfa3ca16a4ae94fd9
bradzara <bradzara93@gmail.com>
2024-04-18 10:45:12 -0700

    Create 341.rb

---
+++ 341.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = {"First name" => "Bradley", "Last name" => "Zara", "Email" => "bradzara93@gmail.com"}
+pp person["First name"]
+pp person["Last name"]
+pp person["Email"]
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I remembered how to create a hash from memory, 
+# but forgot that I need to write three seperate print statements to get each attribute on a seperate line
+
