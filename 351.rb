0fdec777bb2efc8e8561752a4703c30507adb407
bradzara <bradzara93@gmail.com>
2024-04-21 08:43:13 -0700

    Create 351.rb

---
+++ 351.rb
@@ -0,0 +1,24 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+people = [
+  { "First name" => "John", "Last name" => "Smith",},
+  {"First name" => "Robert", "Last name" => "Allen"},
+  {"First name" => "Alice", "Last name" => "Wright"}
+]
+
+pp people[0]["First name"]
+pp people[0]["Last name"]
+
+# I remembered how to do this from memory but the first time around i printed the results as 'pp person[0]'
+# which printed out the desired outcome with the keys as well
+# Next, I will continue with hashes
