61ecf1aaa2e42c9d0b96380bb91da4977f9ebec7
bradzara <bradzara93@gmail.com>
2024-04-23 01:03:44 -0700

    Create 412.rb

---
+++ 412.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Write a method that takes in an array of numbers and returns its sum.
+# Then run the method and print the result.
+
+def sum(array)
+  return array.sum
+end
+
+numbers = [3, 5, 1, 9]
+pp sum(numbers)
