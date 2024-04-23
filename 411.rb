96332b65ed2b22a1bb4813cf7459d189bc04a029
bradzara <bradzara93@gmail.com>
2024-04-23 00:44:55 -0700

    Create 411.rb

---
+++ 411.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+
+# Make a hash to store prices for 3 different menu items. 
+# Then add a new menu item and price and print the hash to see the result.
+
+menu = {"Bread" => "2.99", "Milk" => "2.50", "Sugar" => "4.99"}
+
+menu["Eggs"] = "3.50"
+
+pp menu
+
+# I Forgot how to add a key and value to a hash
+# Tomorrow I will keep practicing hashes
