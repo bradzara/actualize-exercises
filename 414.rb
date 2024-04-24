c372dcc585659e16f07828ac4c07fa436b87ace5
bradzara <bradzara93@gmail.com>
2024-04-23 21:28:21 -0700

    Create 414.rb

---
+++ 414.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+# Read about the Ruby File class: https://rubyapi.org/3.2/o/s?q=file
+# Then write a program that creates a file called example.txt with the 
+# sentence "This is an example!" in it.
+
+file = File.open("example.txt", "w")
+
+file.write("This is an example!")
+
+file.close
+
