47b8375b68444604af4e5325f4d637453769ad75
bradzara <bradzara93@gmail.com>
2024-04-25 10:32:04 -0700

    Create 431.rb

---
+++ 431.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+books = [
+  {title: "No Country for Old Men", author: "Cormac McCarthy"},
+  {title: "Invisible Monsters", author: "Chuck Palahniuk"},
+  {title: "A New Earth", author: "Eckhart Tolle"}
+]
+
+pp books[2][:author]
+
+# I remembered this from memory
+# Although I did forget to put the : in front of author when I called it initially
+# Gonna move onto methods tomorrow
