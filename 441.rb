64faff35527813a5162e954ef21ab4af7ea9c47a
bradzara <bradzara93@gmail.com>
2024-04-27 12:30:24 -0700

    Create 441.rb

---
+++ 441.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Make a hash to store 3 different states and their capitals. Then add a new state and capital and print the hash to see the results
+
+territories = {"New Jersey" => "Trenton", "Pennsylvania" => "Harrisburg", "California" => "Sacramento"}
+
+territories["New York"] = "Albany"
+
+pp territories
+
+# I used the push method at first to add a new entry, so I had to go back and refresh adding a hash
+# Gonna finish up practicing hashes this weekend so I can move on to methods on Monday
