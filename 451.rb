1992cb2b7b3140c20256b5f9d1dc56931ba180ce
bradzara <bradzara93@gmail.com>
2024-04-27 19:57:16 -0700

    Create 451.rb

---
+++ 451.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+laptop = {Brand: "Apple", Model: "Macbook Air", Year: 2022}
+
+puts laptop[:Brand]
+puts laptop[:Model]
+puts laptop[:Year]
+
+# I remembered this from memory
+# Moving onto methods
