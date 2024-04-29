b04961383bad60940134be6a92cbfefa84b34c27
bradzara <bradzara93@gmail.com>
2024-04-29 09:31:59 -0700

    Create 511.rb

---
+++ 511.rb
@@ -0,0 +1,25 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Write a Person class with attributes for name and age.
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+person_1 = Person.new("Brad", 31)
+
+pp person_1
+
+# I remembered this from memory
+# Today, I am going to finish the basic deliberate practice problems and move onto the intermediate ones
