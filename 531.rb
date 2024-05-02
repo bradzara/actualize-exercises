c44b3b2ec0bb1271afe1d6d0af1f0e1b35821f15
bradzara <bradzara93@gmail.com>
2024-05-02 09:35:10 -0700

    Create 531.rb

---
+++ 531.rb
@@ -0,0 +1,19 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+first_name = "Bradley"
+last_name = "Zara"
+
+pp "My name is #{first_name} #{last_name}"
+
+# I remembered from memory
+# continuing with intermediate variables
