f388b4cdfbf798f517e2190212749d14e02de5af
bradzara <bradzara93@gmail.com>
2024-05-04 12:27:08 -0700

    Create 551.rb

---
+++ 551.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+# Write a program that uses variables to store three different colors, 
+# then prints out a sentence using the colors with string concatenation (the + operator).
+
+color1 = "red"
+color2 = "white"
+color3 = "blue"
+
+puts "The colors of the American flag are " + color1 + ", " + color2 + ", and " + color3 + "."
+
+# I remembered this from memory
+# moving on to intermediate arrays next
