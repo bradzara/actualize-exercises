73b93ba1cb3abb52241f8266fcda14ea8af0964a
bradzara <bradzara93@gmail.com>
2024-05-03 23:30:11 -0700

    Create 543

---
+++ 543.js
@@ -0,0 +1,22 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+
+var numbers = [1, 5, 6, 13, 54];
+console.log(numbers[4]);
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+
+var number = (10);
+if (number < 10){
+  console.log(-1);
+} else if (number > 10){
+  console.log (1);
+} else {
+  console.log (0);
+}
